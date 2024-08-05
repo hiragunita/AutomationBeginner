037abeb2937e9241c7b0c6e13b6de30d7e19e349
hiragunita1234@gmail.com <hiragunita1234@gmail.com>
2024-08-06 00:49:17 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,28 +1,34 @@
 package demo;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    Wrappers actions;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
+     * TODO: Write your tests here with testng @Test annotation.
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
 
-     
+    
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
@@ -32,7 +38,8 @@ public class TestCases {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
+        //WebDriverManager.chromedriver().timeout(30).setup();
+        //WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
@@ -42,11 +49,89 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
-
         driver.manage().window().maximize();
+
+        actions = new Wrappers(driver);
+        //WebDriverWait wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void testCase01() throws InterruptedException{
+        //Navigate to this google form.
+        actions.navigateToUrl("https://forms.gle/wjPkzeSEk1CM7KgGA");
+        actions.fillTextbox(By.xpath("(//input[@jsname ='YPqjbf' and @type = 'text'])[1]"), "Crio Learner");
+
+        //Write down "I want to be the best QA Engineer! 1710572021'' where 1710572021 is variable - needs to be the current epoch time.
+        String epoch = String.valueOf(Instant.now().getEpochSecond());
+        actions.textwhyQA(By.xpath("//textarea[@jsname ='YPqjbf']"), "I want to be the best QA Engineer! " + epoch);
+
+        //Enter your Automation Testing experience in the next radio button
+        actions.basicSelection(By.xpath("//span[text() = '0 - 2']"));//div[@class ='bzfPab wFGF8']//span[text() = '0 - 2']
+        Thread.sleep(1000);
+        System.out.println("Echo2");
+
+        //Select Java, Selenium and TestNG from the next check-box
+        actions.basicSelection(By.xpath("//span[text() = 'Java']"));
+        Thread.sleep(1000);
+        actions.basicSelection(By.xpath("//span[text() = 'Selenium']"));
+        Thread.sleep(1000);
+        actions.basicSelection(By.xpath("//span[text() = 'TestNG']"));
+        Thread.sleep(1000);
+
+        //Provide how you would like to be addressed in the next dropdown
+        // WebElement dropdownSelects = driver.findElement(By.xpath("//div[@jsname = 'LgbsSe']"));
+        // Thread.sleep(15000);
+        // actions.clickOnElement(dropdownSelects);
+        // Thread.sleep(15000);
+        // actions.selectAddressuse("Ms");
+        // Thread.sleep(3000);
+        actions.selectAddressuse("Ms");
+        System.out.println("Echo3");
+        
+        // WebElement dropdownSelects = driver.findElement(By.xpath("//div[@jsname = 'LgbsSe']"));
+        // dropdownSelects.click();
+        // JavascriptExecutor js = (JavascriptExecutor) driver;
+        // js.executeScript("arguments[0].click();",dropdownSelects);
+        // WebElement dropdownSelects = driver.findElement(By.xpath("//div[@jsname = 'LgbsSe']"));
+        // actions.clickOnElement(dropdownSelects);
+        // System.out.println("Echo4");
+        // Thread.sleep(15000);
+        // actions.selectAddressuse("Ms");
+        // Thread.sleep(3000);
+        // System.out.println("Echo5");
+
+        //Provided the current date minus 7 days in the next date field, it should be dynamically calculated and not hardcoded.
+        LocalDate currentDate = LocalDate.now();
+        LocalDate pastDate = currentDate.minusDays(7);
+        String requiredDate = pastDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        Thread.sleep(3000);
+        actions.fillTextbox(By.xpath("//input[@type = 'date']"), requiredDate);
+        //System.out.println("Echo6");
+
+        //Provide the time 07:30 in the next field (Can also be in 24 hour clock).
+        actions.selectTime(By.xpath("(//input[@jsname ='YPqjbf'])[3]"), By.xpath("(//input[@jsname ='YPqjbf'])[4]"), "07" , "30");
+        //System.out.println("Echo7");
+        Thread.sleep(3000);
+        
+        //Submit the form.
+        actions.submitButton(By.xpath("//span[text() = 'Submit']"));
+        Thread.sleep(3000);
+        //System.out.println("Echo8");
+
+        //You will see a success message on the website. Print the same message on the console upon successful completion.
+        //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement successMessage = driver.findElement(By.xpath("//div[text() = 'Thanks for your response, Automation Wizard!']"));
+        Thread.sleep(3000);
+        //System.out.println("Echo9");
+        String submissionMessage  = actions.getText(successMessage);
+        Assert.assertEquals(submissionMessage, "Thanks for your response, Automation Wizard!");
+        Thread.sleep(3000);
+
+        System.out.println("Form submitted successfully: " + submissionMessage);
+        //System.out.println("Echo10");
     }
 
     @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,19 +1,189 @@
 package demo.wrappers;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import org.openqa.selenium.Alert;
+import java.time.Duration;
+import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+    ChromeDriver driver;
+    WebDriverWait wait;
+
+    public Wrappers(ChromeDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void navigateToUrl(String url) {
+        driver.get(url);
+    }
+
+    public void fillTextbox(By locator, String text) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public void textwhyQA(By locator, String textQA) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        element.clear();
+        element.sendKeys(textQA);
+    }
+
+    public void basicSelection(By locator) {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        if (!element.isSelected()) {
+            element.click();
+        }
+    }
+
+    public void selectAddressuse(String text) {
+        try {
+            WebElement addressElement = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='MocG8c HZ3kWc mhLiyf LMgvRb KKjvXb DEh1R']")));
+            addressElement.click();
+            WebElement addressDropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='OA0qNb ncFHed QXL7Te']")));
+            List<WebElement> addressOptions = addressDropdown.findElements(By.xpath(".//div/span"));
+            for (WebElement element : addressOptions) {
+                if (element.getText().equalsIgnoreCase(text)) {
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error in selectAddressuse: " + e.getMessage());
+        }
+    }
+
+    public void selectTime(By hourLocator, By minuteLocator, String hour, String minute) {
+        WebElement hourField = wait.until(ExpectedConditions.visibilityOfElementLocated(hourLocator));
+        WebElement minuteField = wait.until(ExpectedConditions.visibilityOfElementLocated(minuteLocator));
+        hourField.sendKeys(hour);
+        minuteField.sendKeys(minute);
+    }
+
+    public void submitButton(By locator) {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+    }
+
+    public String getText(WebElement element) {
+        return element.getText();
+    }
+
+    public void clickOnElement(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
 }
+
+
+// package demo.wrappers;
+
+// import java.util.List;
+
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.JavascriptExecutor;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.WebElement;
+// import org.openqa.selenium.chrome.ChromeDriver;
+
+// public class Wrappers {
+//     ChromeDriver driver;
+
+//     //Use Wrapper for each Action.
+//     public Wrappers(ChromeDriver driver){
+//         this.driver = driver;
+//     }
+
+//     // Navigate to this google form.
+//     public void navigateToUrl(String url){
+//         driver.get(url);
+//     }
+
+//     //Fill in Crio Learner in the 1st text box.
+//     public void fillTextbox(By locator, String text){
+//         WebElement nameBox = driver.findElement(locator);
+//         nameBox.clear();
+//         nameBox.sendKeys(text);
+//     }
+    
+//     //Write down "I want to be the best QA Engineer! 1710572021'' where 1710572021 is variable - needs to be the current epoch time.
+//     public void textwhyQA(By locator, String textQA){
+//         WebElement textwhyQA = driver.findElement(locator);
+//         textwhyQA.clear();
+//         textwhyQA.sendKeys(textQA);
+//     }
+
+//     //Enter your Automation Testing experience in the next radio button and  //Select Java, Selenium and TestNG from the next check-box
+//     public void basicSelection(By locator){
+//         WebElement radioChekboxselection = driver.findElement(locator);
+//         if(!radioChekboxselection.isSelected()){
+//             radioChekboxselection.click();
+//         }
+//     }
+
+//     //Select Java, Selenium and TestNG from the next check-box
+//     // public void checkBoxes(By locator){
+//     //     WebElement checkBoxesselection = driver.findElement(locator);
+//     //     if(!checkBoxesselection.isSelected()){
+//     //         checkBoxesselection.click();
+//     //     }
+//     //}
+
+//     //Provide how you would like to be addressed in the next dropdown
+//     public void selectAddressuse(String text) {
+//         try {
+//             WebElement addressElement = driver.findElement(By.xpath("//div[@class='MocG8c HZ3kWc mhLiyf LMgvRb KKjvXb DEh1R']"));
+//             addressElement.click();
+//             WebElement addressDropdown = driver.findElement(By.xpath("//div[@class='OA0qNb ncFHed QXL7Te']"));
+//             List<WebElement> addressOptions = addressDropdown.findElements(By.xpath(".//div/span"));
+//             for (WebElement element : addressOptions) {
+//                 if (element.getText().equalsIgnoreCase(text)) {
+//                     ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+//                     break;
+//                 }
+//             }
+//         } catch (Exception e) {
+//             System.out.println("Error in selectAddressuse: " + e.getMessage());
+//         }
+//     }
+// //     public void dropDownClick(String dropdownOption) {
+// //         try {
+// //             WebElement dropdownSelection = driver.findElement(By.xpath("(//div[@class='MocG8c HZ3kWc mhLiyf OIC90c LMgvRb' and @data-value='Ms']/span\r\n" + //
+// // ""));
+// //             dropdownSelection.click();
+// //         } catch (Exception e) {
+// //             System.out.println("Error in dropDownClick: " + e.getMessage());
+// //         }
+// //     }
+
+//     //Provided the current date minus 7 days in the next date field, it should be dynamically calculated and not hardcoded.
+
+//     //Provide the time 07:30 in the next field (Can also be in 24 hour clock)
+//     public void selectTime(By hourLocator, By minuteLocator, String hour, String minute) {
+//         WebElement hourField = driver.findElement(hourLocator);
+//         WebElement minuteField = driver.findElement(minuteLocator);
+//         hourField.sendKeys(hour);
+//         minuteField.sendKeys(minute);
+//     }
+//     //Submit the form
+//     public void submitButton(By locator){
+//         driver.findElement(locator).click();
+//     }
+
+//     //You will see a success message on the website. Print the same message on the console upon successful completion
+//     public String getText(WebElement element){
+//         return element.getText();
+//     }
+
+//     public void clickOnElement(WebElement element) {
+//         element.click();
+//     }
+//     /*
+//      * Write your selenium wrappers here
+//      */
+// }
